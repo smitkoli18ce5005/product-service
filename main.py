91a30ddfdb41512a4729586d1f4baae344b5e44d
Smit Koli <koli.smit.18ce5005@gmail.com>
2024-04-20 19:21:24 +0530

    updating update product logic and making use of camel case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -11,34 +12,23 @@ collection = db['product_collection']
 @app.route('/product/add', methods=['POST'])
 def product_add():
     try:
-        product_id = request.form.get('product_id')
-        product_name = request.form.get('product_name')
-        product_description = request.form.get('product_description')
-        product_price = request.form.get('product_price')
-        product_discount_percent = request.form.get('product_discount_percent')
-        product_category = request.form.get('product_category')
-        product_image = request.form.get('product_image')
-
-        if not product_id:
-            return jsonify({'error': 'Product ID is required'}), 400
-
-        if collection.find_one({'product_id': product_id}):
-            return jsonify({'error': 'Product ID already exists'}), 400
+        uniqueId = ObjectId()
 
         product_object = {
-            "product_id": product_id,
-            "product_name": product_name,
-            "product_description": product_description,
-            "product_price": product_price,
-            "product_discount_percent": product_discount_percent,
-            "product_category": product_category,
-            "product_image": product_image
+            "_id": uniqueId,
+            "productId": uniqueId,
+            "productName": request.form.get('productName'),
+            "productDescription": request.form.get('productDescription'),
+            "productPrice": request.form.get('productPrice'),
+            "productDiscountPercent": request.form.get('productDiscountPercent'),
+            "productCategory": request.form.get('productCategory'),
+            "productImage": request.form.get('productImage')
         }
 
         result = collection.insert_one(product_object)
 
         if result.inserted_id:
-            return jsonify({'message': 'Data added successfully', 'product_id': product_id}), 201
+            return jsonify({'message': 'Data added successfully', 'productId': str(uniqueId)}), 201
         else:
             return jsonify({'error': 'Failed to add data'}), 500
     except Exception as e:
@@ -48,36 +38,42 @@ def product_add():
 @app.route('/product/list', methods=['GET'])
 def product_list():
     try:
-        documents = list(collection.find({}, {'_id': 0}))
+        products = list(collection.find())
 
-        return jsonify(documents), 200
+        for product in products:
+            product['_id'] = str(product['_id'])
+            product['productId'] = str(product['productId'])
+
+        return jsonify(products), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/product/list/<product_id>', methods=['GET'])
-def product_list_id(product_id):
+@app.route('/product/list/<productId>', methods=['GET'])
+def product_list_id(productId):
     try:
-        if not product_id:
+        if not productId:
             return jsonify({'error': 'Product ID is required'}), 400
 
-        product = collection.find_one({'product_id': product_id}, {'_id': 0})
+        product = collection.find_one({'productId': ObjectId(productId)})
 
         if not product:
             return jsonify({'error': 'Product not found'}), 404
 
+        product['_id'] = str(product['_id'])
+        product['productId'] = str(product['productId'])
         return jsonify(product), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/product/delete/<product_id>', methods=['DELETE'])
-def delete_product(product_id):
+@app.route('/product/delete/<productId>', methods=['DELETE'])
+def delete_product(productId):
     try:
-        if not product_id:
+        if not productId:
             return jsonify({'error': 'Product ID is required'}), 400
 
-        result = collection.delete_one({'product_id': product_id})
+        result = collection.delete_one({'productId': ObjectId(productId)})
 
         if result.deleted_count == 0:
             return jsonify({'error': 'Product not found'}), 404
@@ -87,30 +83,25 @@ def delete_product(product_id):
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/product/update', methods=['PATCH'])
-def update_product():
+@app.route('/product/update/<productId>', methods=['PATCH'])
+def update_product(productId):
     try:
-        product_id = request.form.get('product_id')
-        product_name = request.form.get('product_name')
-        product_description = request.form.get('product_description')
-        product_price = request.form.get('product_price')
-        product_discount_percent = request.form.get('product_discount_percent')
-        product_category = request.form.get('product_category')
-        product_image = request.form.get('product_image')
-        if not product_id:
+        if not productId:
             return jsonify({'error': 'Product ID is required'}), 400
 
-        updated_data = {
-            "product_id": product_id,
-            "product_name": product_name,
-            "product_description": product_description,
-            "product_price": product_price,
-            "product_discount_percent": product_discount_percent,
-            "product_category": product_category,
-            "product_image": product_image
-        }
+        product = collection.find_one({'productId': ObjectId(productId)})
 
-        result = collection.update_one({'product_id': product_id}, {'$set': updated_data})
+        if not product:
+            return jsonify({'error': 'Product not found'}), 404
+
+        product['productName'] = request.form.get('productName') or product['productName']
+        product['productDescription'] = request.form.get('productDescription') or product['productDescription']
+        product['productPrice'] = request.form.get('productPrice') or product['productPrice']
+        product['productDiscountPercent'] = request.form.get('productDiscountPercent') or product['productDiscountPercent']
+        product['productCategory'] = request.form.get('productCategory') or product['productCategory']
+        product['productImage'] = request.form.get('productImage') or product['productImage']
+
+        result = collection.update_one({'productId': product['productId']}, {'$set': product})
 
         if result.modified_count == 0:
             return jsonify({'error': 'Product contains same data or is not found'}), 404
